738f09ba1a656af5a433964b8ce548c92c3c6fe4
mamahaha125 <993782620@qq.com>
2023-10-14 01:16:46 +0800

    first commit

---
+++ .umirc.ts
@@ -0,0 +1,15 @@
+import { defineConfig } from 'dumi';
+
+const repo = 'sliver-gui'; // 项目名(也就是你的仓库名)
+
+export default defineConfig({
+  title: 'dumi-book-test',
+  mode: 'site',
+  devServer: {
+    port: 1998 // 自定义端口号
+  },
+  base: process.env.NODE_ENV === 'production' ? `/${repo}/` : '/',
+  publicPath: process.env.NODE_ENV === 'production' ? `/${repo}/` : '/',
+  
+  // 更多配置项config: https://d.umijs.org/config  
+});
